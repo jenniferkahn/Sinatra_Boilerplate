76a5552ec34e97676ab38fd57ae0dec737e945c8
Jen Kahn <jenniferhkahn@gmail.com>
2015-11-19 14:25:45 -0600

    sinatra boilerplate

---
+++ app.rb
@@ -0,0 +1,25 @@
+require 'bundler'
+Bundler.require
+
+get '/' do
+#
+
+return {
+  :age => 27,
+#:hash=>'hello friends   '
+#
+}.to_json
+
+end
+
+get '/hash' do
+
+jen_test={
+  :hair => brown
+
+# name_something ={
+# :type => answer
+}
+return jen_test.to_json
+#name_something.to_json
+end
