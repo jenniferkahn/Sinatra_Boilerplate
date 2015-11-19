799a2b316930a4a218b49cd24f0365fca76cd95a
Jen Kahn <jenniferhkahn@gmail.com>
2015-11-19 15:05:28 -0600

    add dictionary

--- app.rb
+++ app.rb
@@ -2,24 +2,29 @@ require 'bundler'
 Bundler.require
 
 get '/' do
+  erb :hello
+end
 #
 
-return {
-  :age => 27,
-#:hash=>'hello friends   '
+# return {
+# #   :age => 27,
+# # #:hash=>'hello friends   '
+# # #
+# # }.to_json
 #
-}.to_json
-
-end
-
-get '/hash' do
-
-jen_test={
-  :hair => brown
-
-# name_something ={
-# :type => answer
-}
-return jen_test.to_json
-#name_something.to_json
-end
+# end
+#
+# get '/world' do
+#
+# end
+# # get '/hash' do
+#
+# # jen_test={
+# #   :hair => brown
+# #
+# # # name_something ={
+# # # :type => answer
+# }
+# return jen_test.to_json
+# #name_something.to_json
+# end
---
+++ public/app.js
@@ -0,0 +1,4 @@
+window.onload = function() {
+console.log('the page is ready');
+//we can do it all now
+}
